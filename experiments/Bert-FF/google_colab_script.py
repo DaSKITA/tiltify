a00a134024a024473120c3d5e811bcc0e060d63c
Michael Gebauer | TU <michael.gebauer@tu-berlin.de>
2022-03-10 12:28:43 +0100

    bugfixes

--- experiments/Bert-FF/google_colab_script.py
+++ experiments/Bert-FF/google_colab_script.py
@@ -9,7 +9,7 @@ from rapidflow.experiments.experiment import Experiment
 if __name__ == "__main__":
 
     exp_path = os.path.join(Path.experiment_path, "Bert-FF")
-    train, val, test = get_finetuning_datasets(Path.default_dataset_path, BASE_BERT_MODEL, val=True)
+    train, val, test = get_finetuning_datasets(Path.default_dataset_path, BASE_BERT_MODEL, val=True, binary=True)
     experiment = Experiment(title="reannotated_documents_first", experiment_path=exp_path, model_name="Bert-FF")
     experiment.add_objective(BERTBinaryObjective, args=[train, val, test])
     experiment.run(k=1, trials=1, num_processes=1)
