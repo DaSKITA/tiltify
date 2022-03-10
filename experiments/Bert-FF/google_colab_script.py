ea8561abff5b86c6075391ba970e2372f8afe1ae
Michael Gebauer | TU <michael.gebauer@tu-berlin.de>
2022-03-10 11:49:48 +0100

    adjusted restults path

--- experiments/Bert-FF/google_colab_script.py
+++ experiments/Bert-FF/google_colab_script.py
@@ -8,7 +8,7 @@ from rapidflow.experiments.experiment import Experiment
 
 if __name__ == "__main__":
 
-    exp_path = os.path.join(Path.experiment_path, "Bert-FF/results")
+    exp_path = os.path.join(Path.experiment_path, "Bert-FF")
     train, val, test = get_finetuning_datasets(Path.default_dataset_path, BASE_BERT_MODEL, val=True)
     experiment = Experiment(title="reannotated_documents_first", experiment_path=exp_path, model_name="Bert-FF")
     experiment.add_objective(BERTBinaryObjective, args=[train, val, test])
