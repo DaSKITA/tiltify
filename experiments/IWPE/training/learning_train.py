51b7bc1d12859ca98a81a0a28acf9cefad8b6e2f
Michael Gebauer | TU <michael.gebauer@tu-berlin.de>
2023-02-10 16:47:56 +0100

    more ranks in learning curve

--- experiments/IWPE/training/learning_train.py
+++ experiments/IWPE/training/learning_train.py
@@ -2,6 +2,7 @@ from sklearn.metrics import classification_report
 from tiltify.models.gaussian_nb_model import GaussianNBModel
 from tiltify.models.sentence_bert import SentenceBert
 from tiltify.models.test_model import TestModel
+from tiltify.models.binary_bert_model import BinaryBERTModel
 from collections import defaultdict
 from tiltify.config import Path
 import os
@@ -19,29 +20,30 @@ def get_documents(document_collection, train_doc_size):
     return document_collection[:(doc_cnt-1)]
 
 
-def eval_model(model, doc_set, ranker):
+def eval_model(model, doc_set, k_ranks):
+    print(f"Starting evaluation of {model.__class__.__name__}...")
+    metrics_dict = {}
     found_doc = []
     real_doc = []
-    for document in tqdm(doc_set):
-        logits = model.predict(document)
-        doc_indexes, _ = ranker.form_k_ranks(logits)
+    for document in tqdm(doc_set[:5]):
         labels = model.preprocessor.label_retriever.retrieve_labels(document.blobs)
         labels = model.preprocessor.prepare_labels(labels)
+        logits = model.predict(document)
         log_based_preds = [logit > 0.5 for logit in logits]
-        found_blob = sum([labels[index] > 0 for index in doc_indexes]) > 0
-        real_blob = sum(labels) > 0
-        found_doc.append(found_blob)
-        real_doc.append(real_blob)
-    krank_metrics = classification_report(real_doc, found_doc, output_dict=True, digits=2)
-    classify_metrics = classification_report(labels, log_based_preds, output_dict=True, digits=2)
-    return {
-        "k_rank_metrics": krank_metrics,
-        "classify_metrics": classify_metrics
-    }
+        metrics_dict["classify_metrics"] = classification_report(labels, log_based_preds, output_dict=True, digits=2)
+        for k_rank in k_ranks:
+            ranker = KRanker(k_rank)
+            doc_indexes, _ = ranker.form_k_ranks(logits)
+            found_blob = sum([labels[index] > 0 for index in doc_indexes]) > 0
+            real_blob = sum(labels) > 0
+            found_doc.append(found_blob)
+            real_doc.append(real_blob)
+        metrics_dict[f"{k_rank}_k_rank_metrics"] = classification_report(real_doc, found_doc, output_dict=True, digits=2)
+    return metrics_dict
 
 
 config = {
-    "k_ranks": 5,
+    "k_ranks": [5, 10, 25],
     "label": "Right to Withdraw Consent",
     "repitions": 2,
     "step_size": 2,
@@ -65,9 +67,9 @@ print(f"Corpus having: {len(test_docs)} Test Docs and {len(train_docs)} Train Do
 model_types = [
     TestModel,
     GaussianNBModel,
-    SentenceBert
+    SentenceBert,
+    BinaryBERTModel
     ]
-ranker = KRanker(config["k_ranks"])
 
 for model_type in model_types:
     print(f"Conducting experment for {model_type.__name__}...")
@@ -88,8 +90,8 @@ for model_type in model_types:
 
             train_set = get_documents(train_docs, train_doc_size)
             model.train(train_set)
-            train_report = eval_model(model, train_set, ranker)
-            test_report = eval_model(model, test_set, ranker)
+            train_report = eval_model(model, train_set, config["k_ranks"])
+            test_report = eval_model(model, test_set, config["k_ranks"])
             model.save(save_dir)
 
             # TODO: add more metrics and maybe also logits
